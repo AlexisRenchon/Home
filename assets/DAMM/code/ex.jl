dbc18fcd43b69ccd6ae33ea49e8a7892c703819d
AlexisRenchon <a.renchon@gmail.com>
2021-09-30 17:58:37 +0000

    Deploying to gh-pages from  @ 0ff237ff743f1adb3164d6159c23cc7478dd7c2f 🚀

--- assets/DAMM/code/ex.jl
+++ assets/DAMM/code/ex.jl
@@ -37,7 +37,7 @@ function create_plot(sliders1, sliders2, sliders3, sliders4, sliders5, sliders6)
 
 	# remove these 3 lines when back to Axis3
 	scene3D = ax3D.scene
-	scale!(scene3D, 1,150, 7) 
+	scale!(scene3D, 3, 150, 7) 
 	center!(scene3D)
 
 
@@ -49,39 +49,50 @@ function create_plot(sliders1, sliders2, sliders3, sliders4, sliders5, sliders6)
 	#scale!(ax3D, 1, 40, 1) 
 	#center!(ax3D)
 
-	p = [1,20,50,0.7]
+	#p = [1,20,50,0.7]
 	
 	ax2D = Axis(fig[1,2])
 	ts1 = collect(10:1:35)
-	lines!(ax2D, ts1, lift(sm -> DAMM(
-	 hcat(ts1, collect(range(sm, length=length(ts1), stop=sm))), p),
-	 sliders5.value), color = :black, linewidth = 8)
+	lines!(ax2D, ts1, lift((AlphaSx, kMSx, kMO2, Porosity, sm) -> DAMM(
+	 hcat(ts1, collect(range(sm, length=length(ts1), stop=sm))), [AlphaSx, kMSx, kMO2, Porosity]),
+	 sliders1.value, sliders2.value, sliders3.value, sliders4.value, sliders5.value), color = :black, linewidth = 8)
 	
 	ax2D2 = Axis(fig[2,2])
 	sm2 = collect(0.0:0.02:0.7)	
-	lines!(ax2D2, sm2, lift(ts -> DAMM(
-	 hcat(collect(range(ts, length=length(sm2), stop=ts)), sm2), p),
-	 sliders6.value), color = :black, linewidth = 8)
+	lines!(ax2D2, sm2, lift((AlphaSx, kMSx, kMO2, Porosity, ts) -> DAMM(
+	 hcat(collect(range(ts, length=length(sm2), stop=ts)), sm2), [AlphaSx, kMSx, kMO2, Porosity]),
+	 sliders1.value, sliders2.value, sliders3.value, sliders4.value, sliders6.value), color = :black, linewidth = 8)
+
+	# TEST: isoline in the 3D figure
+	#lines!(ax3D, lift((AlphaSx, kMSx, kMO2, Porosity, sm) -> ts1, collect(range(sm, length=length(ts1), stop=sm)), DAMM(
+	#hcat(ts1, collect(range(sm, length=length(ts1), stop=sm))), [AlphaSx, kMSx, kMO2, Porosity]),
+	#sliders1.value, sliders2.value, sliders3.value, sliders4.value, sliders5.value), color = :blue, linewidth = 8)
+
 	
-	ylims!(ax2D, 0.0, 30.0)
-	ylims!(ax2D2, 0.0, 30.0)
+	ylims!(ax2D, 0.0, 30.0); xlims!(ax2D, 10.0, 35.0);
+	ylims!(ax2D2, 0.0, 30.0); xlims!(ax2D2, 0.0, 0.8);
 	ax2D.xlabel = to_latex("T_{soil} (°C)");
 	ax2D.ylabel = to_latex("R_{soil} (\\mumol m^{-2} s^{-1})");
 	ax2D2.ylabel = to_latex("R_{soil} (\\mumol m^{-2} s^{-1})");
 	ax2D2.xlabel = to_latex("\\theta (m^3 m^{-3})");
+<<<<<<< HEAD
+	FZ = 30; ax2D.xlabelsize = FZ; ax2D.ylabelsize = FZ; ax2D2.xlabelsize = FZ; ax2D2.ylabelsize = FZ;
+	ax2D.xticklabelsize = FZ; ax2D.yticklabelsize = FZ; ax2D2.xticklabelsize = FZ; ax2D2.yticklabelsize = FZ;
+=======
 	
 	fontsize_theme = Theme(fontsize = 80)
 	set_theme!(fontsize_theme)
+>>>>>>> 4750cf87b180076696da09c3e5a705acf3db8943
 	
 	return fig
 end
 
-sr = [0.1:0.1:0.6, # alpha
-	7:1:50, # kMsx
-	0:1:50, # kmo2
-	0.2:0.05:0.6, # porosity
-	0.0:0.02:0.7, # soil moisture
-	10:1:35]; # soil temperature
+sr =   [0.5:0.1:1, # alpha
+	0.0001:0.5:30, # kMsx
+	0.0001:3:100, # kmo2
+	0.5:0.05:0.7, # porosity
+	0.4:0.02:0.7, # soil moisture
+	30:1:35]; # soil temperature
 
 app = App() do session::Session    
 	sliders1 = JSServe.Slider(sr[1])
@@ -91,12 +102,12 @@ app = App() do session::Session
 	sliders5 = JSServe.Slider(sr[5])
 	sliders6 = JSServe.Slider(sr[6])
 	fig = create_plot(sliders1, sliders2, sliders3, sliders4, sliders5, sliders6)    
-    	slider1 = DOM.div("alpha: ", sliders1, sliders1.value)
-	slider2 = DOM.div("kMsx: ", sliders2, sliders2.value)
-	slider3 = DOM.div("kMO2: ", sliders3, sliders3.value)
-	slider4 = DOM.div("porosity: ", sliders4, sliders4.value)
-	slider5 = DOM.div("soil moisture: ", sliders5, sliders5.value)
-	slider6 = DOM.div("soil temperature: ", sliders6, sliders6.value)
+    	slider1 = DOM.div("Temperature sensitivity, alpha: ", sliders1, sliders1.value)
+	slider2 = DOM.div("Moisture limitation, kMsx: ", sliders2, sliders2.value)
+	slider3 = DOM.div("Oxygen limitation, kMO2: ", sliders3, sliders3.value)
+	slider4 = DOM.div("Porosity: ", sliders4, sliders4.value)
+	slider5 = DOM.div("isoline: fixed soil moisture (%): ", sliders5, sliders5.value)
+	slider6 = DOM.div("isoline: fixed soil temperature (C): ", sliders6, sliders6.value)
     	return JSServe.record_states(session, DOM.div(slider1, slider2, slider3, slider4, slider5, slider6, fig))
 end
 
